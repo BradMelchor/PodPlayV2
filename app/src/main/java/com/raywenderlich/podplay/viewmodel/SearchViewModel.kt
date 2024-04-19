08b7ac271d2f69124e27431f758f69ffa8a31313
BradMelchor <bradley.melchor@gmail.com>
2024-04-18 22:39:08 -0400

    Adding 24-27

--- app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
@@ -6,28 +6,26 @@ import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.PodcastResponse
 import com.raywenderlich.podplay.util.DateUtils
 
-class SearchViewModel (application: Application) :
-        AndroidViewModel(application) {
+class SearchViewModel(application: Application) : AndroidViewModel(application) {
+
     var iTunesRepo: ItunesRepo? = null
 
     data class PodcastSummaryViewData(
         var name: String? = "",
         var lastUpdated: String? = "",
         var imageUrl: String? = "",
-        var feedUrl: String? = ""
-    )
+        var feedUrl: String? = "")
 
     private fun itunesPodcastToPodcastSummaryView(
-        itunesPodcast: PodcastResponse.ItunesPodcast
-    ):
+        itunesPodcast: PodcastResponse.ItunesPodcast):
         PodcastSummaryViewData {
         return PodcastSummaryViewData(
             itunesPodcast.collectionCensoredName,
             DateUtils.jsonDateToShortDate(itunesPodcast.releaseDate),
-            itunesPodcast.artworkUrl30,
-            itunesPodcast.feedUrl
-        )
+            itunesPodcast.artworkUrl100,
+            itunesPodcast.feedUrl)
     }
+
     suspend fun searchPodcasts(term: String): List<PodcastSummaryViewData> {
         val results = iTunesRepo?.searchByTerm(term)
 
